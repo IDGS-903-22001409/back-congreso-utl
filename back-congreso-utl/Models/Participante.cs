8627260142beaa855ba4ae853efb82349470bf78
IDGS-903-22001409 <81347@alumnos.utleon.edu.mx>
2025-11-08 21:37:46 -0600

    tabla

--- back-congreso-utl/Models/Participante.cs
+++ back-congreso-utl/Models/Participante.cs
@@ -1,29 +1,26 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace back_congreso_utl.Models
 {
+    [Table("participantes")]
     public class Participante
     {
         public int Id { get; set; }
 
-        [Required]
-        [StringLength(100)]
+        [Required, StringLength(100)]
         public string Nombre { get; set; } = string.Empty;
 
-        [Required]
-        [StringLength(100)]
+        [Required, StringLength(100)]
         public string Apellidos { get; set; } = string.Empty;
 
-        [Required]
-        [EmailAddress]
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
 
-        [Required]
-        [StringLength(50)]
+        [Required, StringLength(50)]
         public string Twitter { get; set; } = string.Empty;
 
-        [Required]
-        [StringLength(100)]
+        [Required, StringLength(100)]
         public string Ocupacion { get; set; } = string.Empty;
 
         public string? Avatar { get; set; }
